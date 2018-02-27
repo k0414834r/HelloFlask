4f04f85e79bd1be93b55063fe8c22e503c82cfdd
Boian Etropolski <boetropolski@gmail.com>
2018-02-27 13:15:41 -0600

    Dictionary now readable

--- hello.py
+++ hello.py
@@ -6,6 +6,18 @@ app = Flask(__name__)
 @app.route('/index')
 def index():
     user1 = {'username': 'Boian'}
-    return render_template('index.html', user = user1)
+
+    posts = [
+        {
+            'title': {'main': 'I hate Python', 'sub': 'this is a sad story'},
+            'body': 'Python can do every thing. However, it is very hard.'
+        },
+        {
+            'title': {'main': 'I love Python', 'sub': 'this is a happy story'},
+            'body': 'Python can do every thing. However, it is very easy.'
+        }
+    ]
+
+    return render_template('index.html', user = user1, posts = posts)
 
 app.run()
