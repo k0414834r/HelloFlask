b2c7059213c9944266cab3c3d7e4dd114e7cb9aa
Boian Etropolski <boetropolski@gmail.com>
2018-02-27 14:11:06 -0600

    added store.html

--- hello.py
+++ hello.py
@@ -10,22 +10,45 @@ def index():
     posts = [
         {
             'title': {'main': 'I hate Python', 'sub': 'this is a sad story'},
-            'body': 'Python can do every thing. However, it is very hard.'
+            'price': 'Python can do every thing. However, it is very hard.'
         },
         {
             'title': {'main': 'I love Python', 'sub': 'this is a happy story'},
-            'body': 'Python can do every thing. However, it is very easy.'
+            'price': 'Python can do every thing. However, it is very easy.'
         },
         {
             'title': {'main': 'I love C#', 'sub': 'this is a neutral story'},
-            'body': 'C# can do something, and it is very easy.'
+            'price': 'C# can do something, and it is very easy.'
         },
         {
             'title': {'main': 'I hate C#', 'sub': 'this is a neutral story'},
-            'body': 'C# cannot do anything, and it is stupid!'
+            'price': 'C# cannot do anything, and it is stupid!'
         }
     ]
 
-    return render_template('index.html', user = user1, posts = posts, title = "HelloFlask")
+    return render_template('index.html', user=user1, posts=posts, title="HelloFlask")
+
+@app.route('/store')
+def store():
+    items = [
+        {
+            'title': "Python book",
+            'price': 200
+        },
+        {
+            'title': "Cook book",
+            'price': 20
+        },
+        {
+            'title': "Samsung Note 5",
+            'price': 1000
+        },
+        {
+            'title': "Apartment, monthly",
+            'price': 1350
+        }
+    ]
+
+    return render_template('store.html', items = items)
 
 app.run()
