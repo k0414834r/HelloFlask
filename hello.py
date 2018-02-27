93b10eee53f14277f9b74a5701a45be76a943be1
Boian Etropolski <boetropolski@gmail.com>
2018-02-27 11:46:48 -0600

    pass var to jinja & template fix w/ sub

--- hello.py
+++ hello.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    user1 = {'username': 'Boian'}
+    return render_template('index.html', user = user1)
 
 app.run()
