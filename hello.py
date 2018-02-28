a0641897689b1dd53e21f8519b667d6de4a1aa44
Boian Etropolski <boetropolski@gmail.com>
2018-02-28 11:35:24 -0600

    login redirect + flash

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect
 from forms import LoginForm
 
 app = Flask(__name__)
@@ -56,6 +56,10 @@ def store():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me = {}'.format(form.username.data, form.remember_me.data))
+        return redirect('/login')
+
     return render_template('login.html', title = 'Sign In', form = form)
 
 app.run()
