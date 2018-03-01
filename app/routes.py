bed72a7bdea2ba5e98f2f2eea1f06a13868aa515
Boian Etropolski <boetropolski@gmail.com>
2018-03-01 13:46:42 -0600

    create registration.html

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app2 as app
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrationForm
 from flask import Flask, render_template, flash, redirect, url_for
 from flask_login import current_user, login_user, logout_user
 from app.models import Post, User
@@ -61,4 +61,20 @@ def logout():
 
 @app.route('/register', methods = ['GET', 'POST'])
 def register():
-    return "You have been registered"
+    if current_user.is_authenticated:
+        return "You have been registered"
+
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+
+        db.session.add(user)
+        db.session.commit()
+
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+
+    return render_template('register.html', title='Register', form=form)
+
+
